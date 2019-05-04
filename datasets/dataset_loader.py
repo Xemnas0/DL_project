8f8a4c233e6ddf332fb51da216b4a5063c840ba4
Xemnas0 <franuzzo966@gmail.com>
2019-05-04 21:48:17 +0200

    added one hot encoding

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -1,5 +1,6 @@
 import tensorflow_datasets as tfds
 from tensorflow.python import keras
+import tensorflow as tf
 import numpy as np
 
 EXISTING_DATASETS = ['MNIST', 'CIFAR10', 'CIFAR100']
@@ -13,17 +14,23 @@ def load_dataset(dataset_name):
         x_train = x_train[..., np.newaxis]
         x_train = x_train.astype('float32') / 255
         x_test = x_test.astype('float32') / 255
+        Y_train = tf.one_hot(y_train, y_train.max()+1, dtype=tf.int32)
+        Y_test = tf.one_hot(y_test, y_test.max()+1, dtype=tf.int32)
     elif dataset_name == 'CIFAR10':
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
         x_train = x_train.astype('float32')
         x_test = x_test.astype('float32')
+        Y_train = tf.one_hot(y_train[:, 0], y_train.max() + 1, dtype=tf.int32)
+        Y_test = tf.one_hot(y_test, y_test.max()+1, dtype=tf.int32)
     elif dataset_name == 'CIFAR100':
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
         x_train = x_train.astype('float32')
         x_test = x_test.astype('float32')
+        Y_train = tf.one_hot(y_train[:, 0], y_train.max() + 1, dtype=tf.int32)
+        Y_test = tf.one_hot(y_test, y_test.max()+1, dtype=tf.int32)
     print(f'Dataset {dataset_name} loaded.')
 
-    return (x_train, y_train), (x_test, y_test)
+    return (x_train, y_train, Y_train), (x_test, y_test, Y_test)
 
 
 if __name__ == '__main__':

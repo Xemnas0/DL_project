08b67c9f5b3dbec81ea7eebb117f762c233d7b2c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-12 18:37:35 +0000

    run sh new

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ if [ ! -f main.py ]; then
 fi;
 
 python3 -u main.py \
---epochs 5 \
---P 0.75 \
---C 32 \
---K 4 \
---M 1 \
---seed 0 \
---graph-mode WS \
---N 32 \
---stages 1 \
---learning-rate 1e-2 \
---batch-size 32 \
---regime 'small' \
---dataset 'MNIST' \
---distributed 0 \
+  --epochs 5 \
+  --P 0.75 \
+  --C 32 \
+  --K 4 \
+  --M 1 \
+  --seed 0 \
+  --graph-mode WS \
+  --N 32 \
+  --stages 1 \
+  --learning-rate 0.001 \
+  --batch-size 32 \
+  --regime 'small' \
+  --dataset 'MNIST' \
+  --distributed 1 \

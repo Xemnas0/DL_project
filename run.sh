376d9913c9f6080b960fff3b6df0e0c7a932873e
Xemnas0 <franuzzo966@gmail.com>
2019-05-15 21:19:36 +0200

    param for cifar100

--- run.sh
+++ run.sh
@@ -7,18 +7,21 @@ fi;
 
 python3 -u main.py \
   --epochs 100 \
-  --P 0.75 \
-  --C 64 \
+  --P 0.2 \
+  --C 78 \
   --K 4 \
   --M 1 \
   --seed 0 \
-  --graph-mode 'WS' \
+  --graph-mode 'ER' \
   --N 32 \
   --stages 2 \
   --learning-rate 0.1 \
   --batch-size 128 \
   --regime 'small' \
-  --dataset 'CIFAR10' \
+  --dataset 'CIFAR100' \
   --augmented 1 \
   --decay 5e-9 \
-  --stride 1
+  --stride 1 \
+  --lr_period 50 \
+  --min_lr 1e-6 \
+  --update_type_lr 'batch'

8698d9baa1ed5e533db5cd7bd72c6efc6d5df41f
Chirantan Sensharma <sensharma@gmail.com>
2019-05-17 23:50:01 +0200

    cleaned up

--- model/degree_dist.py
+++ model/degree_dist.py
@@ -4,31 +4,34 @@ import networkx as nx
 
 
 def deg_dist(G):
-    print("In deg dist")
-    all_degrees = dict(G.degree())     #nx.degree(G).values()
+    '''
+    Calculates the degree distribution of one example of each type of random graph
+    :param G: Graph
+    :return: unique degrees, degree counts
+    '''
+
+    all_degrees = dict(G.degree())
     all_degree_values = all_degrees.values()
     unique_degrees = list(set(all_degree_values))
     unique_degrees.sort()
     count_of_degrees = []
 
     for i in unique_degrees:
-        c = list(all_degree_values).count(i)  #c = all_degrees.count(i)
+        c = list(all_degree_values).count(i)
         count_of_degrees.append(c)
 
     print(unique_degrees)
     print(count_of_degrees)
 
-    # plt.plot(unique_degrees, count_of_degrees, 'ro-')
-    # plt.xlabel('Degrees')
-    # plt.ylabel('Number of nodes')
-    # plt.title('Degree Distribution')
-    # plt.show()
     return unique_degrees, count_of_degrees
 
 
-# if __name__ == '__main__()':
 def main():
-    print("In main")
+    '''
+    Calls the generating and degree distribution funcs and does the plots
+    :return: None
+    '''
+
     G1 = gen_ER_graph(0.2, n=10000)
     G2 = gen_BA_graph(10, n=10000)
     G3 = gen_WS_graph(40, 0.75, 10000)
@@ -60,7 +63,6 @@ def main():
     plt.title('Degree Distribution')
     plt.legend()
     plt.show()
-    # plot_deg_dist(G3)
 
 main()
 

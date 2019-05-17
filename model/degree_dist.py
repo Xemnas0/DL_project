73e0b9d51592ddb70c8cb9e18066777a7ba7ef02
Chirantan Sensharma <sensharma@gmail.com>
2019-05-17 23:44:22 +0200

    degree distribution plots

---
+++ model/degree_dist.py
@@ -0,0 +1,67 @@
+from genrandgraphNX import gen_ER_graph, gen_BA_graph, gen_WS_graph
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def deg_dist(G):
+    print("In deg dist")
+    all_degrees = dict(G.degree())     #nx.degree(G).values()
+    all_degree_values = all_degrees.values()
+    unique_degrees = list(set(all_degree_values))
+    unique_degrees.sort()
+    count_of_degrees = []
+
+    for i in unique_degrees:
+        c = list(all_degree_values).count(i)  #c = all_degrees.count(i)
+        count_of_degrees.append(c)
+
+    print(unique_degrees)
+    print(count_of_degrees)
+
+    # plt.plot(unique_degrees, count_of_degrees, 'ro-')
+    # plt.xlabel('Degrees')
+    # plt.ylabel('Number of nodes')
+    # plt.title('Degree Distribution')
+    # plt.show()
+    return unique_degrees, count_of_degrees
+
+
+# if __name__ == '__main__()':
+def main():
+    print("In main")
+    G1 = gen_ER_graph(0.2, n=10000)
+    G2 = gen_BA_graph(10, n=10000)
+    G3 = gen_WS_graph(40, 0.75, 10000)
+
+    u1, c1 = deg_dist(G1)
+    u2, c2 = deg_dist(G2)
+    u3, c3 = deg_dist(G3)
+
+    fig1 = plt.figure(figsize=(5, 3))
+    plt.plot(u1, c1, 'ro-', label='p=0.2, n=10000')
+    plt.xlabel('Degrees')
+    plt.ylabel('Number of nodes')
+    plt.title('Degree Distribution')
+    plt.legend()
+    plt.show()
+
+    fig2 = plt.figure(figsize=(5, 3))
+    plt.plot(u2, c2, 'bo-', label='m=10, n=10000')
+    plt.xlabel('Degrees')
+    plt.ylabel('Number of nodes')
+    plt.title('Degree Distribution')
+    plt.legend()
+    plt.show()
+
+    fig3 = plt.figure(figsize=(5, 3))
+    plt.plot(u3, c3, 'go-', label='k=40, p=0.75, n=10000')
+    plt.xlabel('Degrees')
+    plt.ylabel('Number of nodes')
+    plt.title('Degree Distribution')
+    plt.legend()
+    plt.show()
+    # plot_deg_dist(G3)
+
+main()
+
+
